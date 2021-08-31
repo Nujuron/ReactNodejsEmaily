3b572a3ac94f9da4270cab3098795980a704bbf5
Nujuron <adri.brito@outlook.es>
2021-08-31 10:53:02 +0100

    MongoDB connection

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
+// DB connection
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
 
 const app = express();
 
--- services/passport.js
+++ services/passport.js
@@ -1,15 +1,26 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
-
-// https://console.cloud.google.com/
-passport.use(new GoogleStrategy({ 
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile:', profile);
-    })
-); // new instance of GoogleStrategys
+ 
+const User = mongoose.model('users');
+ 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+        } else {
+          // we don't have a user record with this ID, make a new record!
+          new User({ googleId: profile.id }).save();
+        }
+      });
+    }
+  )
+);
