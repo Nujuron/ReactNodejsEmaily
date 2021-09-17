f7045055fe2ed6f777aa2942ace65e7bd5460e10
Nujuron <adri.brito@outlook.es>
2021-09-17 14:23:54 +0100

    Returns user state

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,9 @@
-export default function authReducer(state = {}, action) {
+import { FECTH_USER } from "../actions/types";
+// returns null when it's loading the request
+export default function authReducer(state = null, action) {
     switch (action.type) {
+        case FECTH_USER:
+            return action.paidload || false; // "" === false, so if it's empty it returns false
         default:
             return state;
     }
