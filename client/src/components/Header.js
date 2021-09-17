7db32b3e8a720f2ac5b13a39d6a36e8cb02c1e1d
Nujuron <adri.brito@outlook.es>
2021-09-17 14:58:11 +0100

    Header v1.0

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux';
 
 class Header extends Component {
+
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login With Google</a></li>;
+            default:
+                return <li><a>Logout</a></li>;
+        }
+    }
+
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
                     <a href="#" className="left brand-logo">Emaily</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="sass.html">Login With Google</a></li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
         );
     }
 }
-
-export default Header;
+// load redux store to props
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+export default connect(mapStateToProps)(Header);
