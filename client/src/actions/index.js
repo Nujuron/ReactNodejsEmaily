e5cc1f400b413da617ea37ee6fcb240621adcff3
Nujuron <adri.brito@outlook.es>
2021-09-17 14:14:22 +0100

    Refactor

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,10 +2,7 @@ import axios from 'axios';
 import { FECTH_USER } from './types';
 
 // reduxThunk handles the function
-export const fetchUser = () => {
-    return function (dispatch) {
-        axios
-            .get('/api/current_user') // dispatch the action after we get a response
-            .then(res => dispatch({ type: FECTH_USER, payload: res }))
-    }
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get('/api/current_user') // dispatch the action after we get a response
+    dispatch({ type: FECTH_USER, payload: res.data })
 };
