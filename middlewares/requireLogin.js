c3737eca90e8edfcc1ad31c305d16f6e189291b6
Nujuron <adri.brito@outlook.es>
2021-09-20 14:11:19 +0100

    Typo fix

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,6 +1,6 @@
 // checks if user is logged in before doing an API request
 module.exports = (req, res, next) => {
-    if( req.user) {
+    if( !req.user) {
         return res.status(401).send({ error: 'You must be log in!'});
     }
     next();
