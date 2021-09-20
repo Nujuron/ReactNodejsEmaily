776d48a802ef216762e82534bc7ea7efdfbd327f
Nujuron <adri.brito@outlook.es>
2021-09-20 14:03:19 +0100

    Checks user middleware

---
+++ middlewares/requireLogin.js
@@ -0,0 +1,7 @@
+// checks if user is logged in before doing an API request
+module.exports = (req, res, next) => {
+    if( req.user) {
+        return res.status(401).send({ error: 'You must be log in!'});
+    }
+    next();
+};
