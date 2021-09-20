9a8beba1ca6448fc4ee96a39a6147f4336c45fd0
Nujuron <adri.brito@outlook.es>
2021-09-20 14:21:49 +0100

    Styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
             default:
                 return [
                     <li key="1"><Payments /></li>,
-                    <li key="2">{this.props.auth.credits} credits</li>,
+                    <li key="2" style={{margin: '0 10px' }}>{this.props.auth.credits} Credits</li>,
                     <li key="3"><a href="/api/logout">Logout</a></li>
                 ];
         }
