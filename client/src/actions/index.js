e8600b44b51f7a6de27d3fbc3a1df337a9253e63
Nujuron <adri.brito@outlook.es>
2021-09-20 11:01:34 +0100

    Stripe API reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,6 +3,11 @@ import { FECTH_USER } from './types';
 
 // reduxThunk handles the function
 export const fetchUser = () => async dispatch => {
-    const res = await axios.get('/api/current_user') // dispatch the action after we get a response
-    dispatch({ type: FECTH_USER, payload: res.data })
+    const res = await axios.get('/api/current_user'); // dispatch the action after we get a response
+    dispatch({ type: FECTH_USER, payload: res.data });
 };
+// handles the token from Stripe API, updates the user, sends back the user info
+export const handleToken = (token) => async dispatch => {
+    const res = await axios.post('/api/stripe', token);
+    dispatch({ type: FECTH_USER, payload: res.data});
+}
