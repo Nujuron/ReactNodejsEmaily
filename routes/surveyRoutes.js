24c5eebde8b4fce5257292b459bb8303b0ca36e7
Nujuron <adri.brito@outlook.es>
2021-09-23 12:53:48 +0100

    survey POST done

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.post('api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
         const { title, subject, body, recipients } = req.body;
         const survey = new Survey({
             title,
@@ -15,5 +17,9 @@ module.exports = app => {
             _user: req.user.id,
             dateSent: Date.now()
         });
+
+        // Send an email
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+        mailer.send();
     });
 };
