613a52518061ce78a5e92ad0571ad29e082c2417
Nujuron <adri.brito@outlook.es>
2021-10-08 14:13:50 +0100

    Post survey action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -11,3 +11,9 @@ export const handleToken = (token) => async dispatch => {
     const res = await axios.post('/api/stripe', token);
     dispatch({ type: FECTH_USER, payload: res.data});
 }
+
+export const submitSurvey = (values, history) => async dispatch => {
+    const res = await axios.post('/api/surveys', values);
+    history.push('/surveys');
+    dispatch({ type: FECTH_USER, payload: res.data });
+}
