fefbcde6ae55a17e4cd89631a80b64db50eaecef
Nujuron <adri.brito@outlook.es>
2021-10-08 13:14:06 +0100

    Refactor emails to recipients

--- client/src/components/Surveys/formFields.js
+++ client/src/components/Surveys/formFields.js
@@ -2,7 +2,7 @@ const FIELDS = [
     {label:'Survey Title', name: 'title' },
     {label:'Survey Subject', name: 'subject'},
     {label:'Email body', name: 'body'},
-    {label:'Recipient List', name: 'emails'},
+    {label:'Recipient List', name: 'recipients'},
 ];
 
 export default FIELDS;
