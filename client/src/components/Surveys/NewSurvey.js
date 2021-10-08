3868dd2c2c6e15507a265c05e7a96f20a18483be
Nujuron <adri.brito@outlook.es>
2021-10-08 12:06:24 +0100

    Added hook to toggle form state

--- client/src/components/Surveys/NewSurvey.js
+++ client/src/components/Surveys/NewSurvey.js
@@ -1,14 +1,38 @@
-import React, {Component} from "react";
+import React from "react";
 import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
-class NewSurvey extends Component {
-    render() {
-        return (
-            <div>
-                <SurveyForm />
-            </div>
-        );
+const useToggle = (initialState) => {
+    const [isToggled, setIsToggled] = React.useState(initialState);
+  
+    // put [setIsToggled] into the useCallback's dependencies array
+    // this value never changes so the callback is not going to be ever re-created
+    const toggle = React.useCallback(
+      () => setIsToggled(state => !state),
+      [setIsToggled],
+    );
+  
+    return [isToggled, toggle];
+}
+
+const NewSurvey = () => {
+    const [showReview, toggle] = useToggle(false);
+
+    function renderContent() {
+        if (showReview) {
+            return <SurveyFormReview 
+                onCancel={toggle}
+            />;
+        }
+        return <SurveyForm onSurveySubmit={toggle} />;
     }
+
+    return (
+        <div>
+            {renderContent()}
+        </div>
+    );
+
 }
 
 export default NewSurvey;
