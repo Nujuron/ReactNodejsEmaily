42f715a64d599a7042fd21a64f92d29623c29cb4
Nujuron <adri.brito@outlook.es>
2021-10-08 14:06:08 +0100

    Fixes to fields' name

--- client/src/components/Surveys/formFields.js
+++ client/src/components/Surveys/formFields.js
@@ -1,6 +1,6 @@
 const FIELDS = [
-    {label:'Survey Title', name: 'title' },
-    {label:'Survey Subject', name: 'subject'},
+    {label:'Campaign Title', name: 'title' },
+    {label:'Subject Line', name: 'subject'},
     {label:'Email body', name: 'body'},
     {label:'Recipient List', name: 'recipients'},
 ];
