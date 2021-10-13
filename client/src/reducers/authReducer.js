eda56754cc68ab31c502b36c441c26a97a81bcdd
Nujuron <adri.brito@outlook.es>
2021-10-13 12:00:31 +0100

    fixed type

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,8 +1,8 @@
-import { FECTH_USER } from "../actions/types";
+import { FETCH_USER } from "../actions/types";
 // returns null when it's loading the request
 export default function authReducer(state = null, action) {
     switch (action.type) {
-        case FECTH_USER:
+        case FETCH_USER:
             return action.payload || false; // "" === false, so if it's empty it returns false
         default:
             return state;
