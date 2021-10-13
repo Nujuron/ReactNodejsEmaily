99a83b25c578512a2bb061d01a1ccdb01b3beb35
Nujuron <adri.brito@outlook.es>
2021-10-13 13:03:49 +0100

    Reverted changes

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,7 +6,7 @@ class Mailer {
     sgMail.setApiKey(keys.sendGridKey);
     this.msg = {
       to: recipients.map(({ email }) => email),
-      from: "Emaily App",
+      from: "archimon99@gmail.com",
       subject: subject,
       html: content,
       trackingSettings: { enable_text: true, enabled: true }
