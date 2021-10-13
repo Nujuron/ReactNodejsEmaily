741e1049c5004ac35fd4267877bac2dfe86b9816
Nujuron <adri.brito@outlook.es>
2021-10-13 12:26:44 +0100

    Changed stuff

--- client/src/components/SurveyList.js
+++ client/src/components/SurveyList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchSurveys} from '../../actions';
+import { fetchSurveys} from '../actions';
 
 class SurveyList extends React.Component {
     componentDidMount() {
